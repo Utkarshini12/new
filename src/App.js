2c8a8f736dbfcafabecf11db1e404015559772ba
Utkarshini12 <arorautkarshini@gmail.com>
2022-05-09 21:43:19 +0530

    h

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import Home from './pages/home'
-
 import { Suspense } from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './App.css';
 
 
